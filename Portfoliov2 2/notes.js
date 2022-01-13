4d5c9668377d98d3cd0d93df14a3fb525c46aa70
Matthew Condon <90637488+MatthewCondon@users.noreply.github.com>
2022-01-13 14:36:59 -0600

    Add files via upload

---
+++ Portfoliov2 2/notes.js
@@ -0,0 +1,80 @@
+<!-- Notebooks -->
+    <div class="flex-container5">
+      <div> <h1> <b> Computer Science Notebooks 
+        <br>
+        <h5 style="font-size:15px">
+         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQbjybP7Ju8q9V6BdqUrsIc3VJdpySo-Vl5zioCH7uvTwjvhEPXwPJHhUibzu9urr01183qk8fx4Mnv/embed?start=false&loop=false&delayms=3000" frameborder="0" width="300" height="209" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vT6EVVRDc86_a5BIZvUQhAb-6RyZ-_FRnuUgcPVek9rHPKfaYkoFu9UV6xE1lnMukXPnrnbFR3srsqB/embed?start=false&loop=false&delayms=3000" frameborder="0" width="300" height="209" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+         <br><br><br>
+         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTGFVKNj22aeSPpXu_dRk-BQM22SSH25B671i44lAk3SRAbGZyEw9kfRAvXjYoiX4081eSchB28B6XP/embed?start=false&loop=false&delayms=3000" frameborder="0" width="300" height="209" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSVJG1KoUO6n3mdPDVejpb-Uf2ro5a4SgkabnrLSf_ay7CX_Z6f8gFuqYmTAM0s4xdPywJNT_lvUXtp/embed?start=false&loop=false&delayms=3000" frameborder="0" width="300" height="209" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+         <br><br><br>
+         <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSuUpihuZylPu6QZy_vGI5mf2g5e2Sk7vS1Lt0xhNLOORyFia2eHAMPX-YFhdWCr1x1oTmhGizBrwXx/embed?start=false&loop=false&delayms=3000" frameborder="0" width="300" height="209" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+        </h5>
+      </div>
+      
+    </div>
+
+
+      <!-- Projects -->
+    <div class="flex-container4">
+      <div> <h1> <b>Computer Science Projects 
+        <br>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/firstHol.html"><img src="/photos/firstHol.png" alt="First Holiday Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 20px; padding-bottom: 30px">First Holidays Card</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/faces1.html"><img src="/photos/faces1.png" alt="First Face Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 55px; padding-bottom: 30px">Faces 1</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/faces2.html"><img src="/photos/faces3.png" alt="Second Face Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 55px; padding-bottom: 30px">Faces 2</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/faces3.html"><img src="/photos/faces2.png" alt="Third Face Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 55px; padding-bottom: 30px">Faces 3</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/bargame.html"><img src="/photos/BarGame.png" alt="Bar Game Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 35px; padding-bottom: 30px">The Bar Game</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/firstclock.html"><img src="/photos/FirstClock.png" alt="First Clock Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 40px; padding-bottom: 30px">Digital Clock</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/scaled1.html"><img src="/photos/Scaled1.png" alt="First Scaled Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 50px; padding-bottom: 30px">Scaled 1</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/scaled2.html"><img src="/photos/Scaled2.png" alt="Second Scaled Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 50px; padding-bottom: 30px">Scaled 2</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/secondPort.html"><img src="/photos/secondPort.png" alt="Second Portfolio Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 35px; padding-bottom: 30px">Second Portfolio</h5></a>
+        </h5>
+      </div>
+      <div>
+        <br><br><br>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/jackproj.html"><img src="/photos/JackProj.png" alt="Jack O' Lantern Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 30px; padding-bottom: 30px">Jack O' Lanterns</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/solarsystem.html"><img src="/photos/SolarSystem.png" alt="Solar System Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 40px; padding-bottom: 30px">Solar System</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/diceproj.html"><img src="/photos/DiceProj.png" alt="Dice Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 60px; padding-bottom: 30px">Dice</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/fireext.html"><img src="/photos/FireExt.png" alt="Fire Extinguisher Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 30px; padding-bottom: 30px">Fire Extinguisher</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/spaceinv.html"><img src="/photos/SpaceInvaders.png" alt="Space Invaders Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 30px; padding-bottom: 30px">Space Invaders</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/newHol.html"><img src="/photos/holiday21proj.png" alt="Second Holiday Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 15px; padding-bottom: 30px">Second Holidays Card</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/newclock.html"><img src="/photos/NewClock.png" alt="New Clock Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 40px; padding-bottom: 30px">Analog Clock</h5></a>
+        </h5>
+        <h5 style="font-size:15px">
+          <a href="/projectPages/firstPort.html"><img src="/photos/FirstPort.png" alt="First Portfolio Project Photo" style="height:150px;width:150px"><br><h5 style="padding-left: 40px; padding-bottom: 30px">First Portfolio</h5></a>
+        </h5>
+      </div>
+      </div>      
+    </div>
+    </div>
